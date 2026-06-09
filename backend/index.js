77426a384a3f4795ae8097bb7006cc103b5e0649
Rushikesh Watane <rushipatilwatane1234@gmail.com>
2026-06-09 17:56:18 +0530

    update path problem

--- backend/index.js
+++ backend/index.js
@@ -27,12 +27,14 @@ app.use(cors({
 app.use('/api/auth',AuthRouter)
 app.use('/api/message',MessageRouter)
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-    app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-})
+  app.use(express.static(frontendPath));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 server.listen(port,()=>{
